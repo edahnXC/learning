ed988c527cc8fadbc9fb332626d40661a2b4359f
edahnXC <edahnxc@gmail.com>
2023-01-26 13:26:28 +0530

    weekdays

--- weekdays.py
+++ weekdays.py
@@ -1,19 +1,18 @@
-day=int(input("enter the day number: "))
-for day in range(0,32):    
-   if day==1:
-      print("it's Sunday")
-   elif day==2:
-      print("it's Monday")
-   elif day==3:
-      print("it's Tuesday")
-   elif day==4:
-      print("it's Wednesday")
-   elif day==5:
-      print("it's Thursday")
-   elif day==6:
-      print("it's Friday")
-   elif day==7:
-      print("it's Saturday")
-   if day==8:
-       day=1
+day=int(input("enter the day number: "))   
+if day in [1,8,15,22,29]:
+   print("it's Sunday")
+elif day in [2,9,16,23,30]:
+   print("it's Monday")
+elif day in [3,10,17,24,31]:
+   print("it's Tuesday")
+elif day in[4,11,18,25]:
+   print("it's Wednesday")
+elif day in[5,12,19,26]:
+   print("it's Thursday")
+elif day in[6,13,20,27]:
+   print("it's Friday")
+elif day in[7,14,21,28]:
+   print("it's Saturday")
+else:
+   print("day entered is not correct")
       
