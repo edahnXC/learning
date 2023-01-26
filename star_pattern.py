c03101a74ad3b5670f18c74d690d8d513d4144de
edahnXC <edahnxc@gmail.com>
2023-01-26 14:20:35 +0530

    table of 256

--- star_pattern.py
+++ star_pattern.py
@@ -2,3 +2,10 @@ for i in range(3):
     for j in range(i+1):
         print("*",end=' ')
     print()
+
+# for i in range(4):
+#     for j in range(2):
+#         print(end=' ')
+#     for j in range(i+1):
+#         print("*",end="")
+#     print()
