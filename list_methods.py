d633e52bf5190fac3b3bb01bb0a1ffb7cbcca244
edahnXC <edahnxc@gmail.com>
2023-01-27 21:31:40 +0530

    list operations

--- list_methods.py
+++ list_methods.py
@@ -15,23 +15,22 @@
 #     else:
 #         print("Error,the command given is wrong")
 # print(a)
-if __name__ == '__main__':
-    N = int(input())
-    list=[]
-    for i in range(N):
-        cmd=input().split()
-        if cmd[0]=="append":
-                list.append(int(cmd[1]))
-        elif cmd[0]=="insert":
-                list.insert(int(cmd[1]),int(cmd[2]))
-        elif cmd[0]=="remove":
-                list.remove(int(cmd[1]))
-        elif cmd[0]=="sort":
-                list.sort()
-        elif cmd[0]=="pop":
-                list.pop()
-        elif cmd[0]=="reverse":
-                list.reverse()
-        else:
-                print("Enter the currect command")
-print(list)
+n = int(input())
+a=[]
+for i in range(n):
+    cmd=input().split()
+    if cmd[0]=="append":
+        a.append(int(cmd[1]))
+    elif cmd[0]=="insert":
+        a.insert(int(cmd[1]),int(cmd[2]))
+    elif cmd[0]=="remove":
+        a.remove(int(cmd[1]))
+    elif cmd[0]=="sort":
+        a.sort()
+    elif cmd[0]=="pop":
+        a.pop()
+    elif cmd[0]=="reverse":
+        a.reverse()
+    else:
+        print("Enter the correct command")
+print(a)
