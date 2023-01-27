c2decb02afcffe11adaad01e01bf7a3ec3ffc43d
edahnXC <edahnxc@gmail.com>
2023-01-27 07:32:47 +0530

    star patterns

--- star_pattern.py
+++ star_pattern.py
@@ -7,6 +7,6 @@ n=int(input("enter the number of rows: "))
 for i in range(1,n+1):
     for j in range(n-i):
         print(' ',end='')
-    for k in range(2*i-1):
+    for k in range(i):
         print('*',end=' ')
     print()
