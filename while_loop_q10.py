72681e812e6ab1037220f92ddf08798b3a5366d1
edahnXC <edahnxc@gmail.com>
2023-01-31 21:58:49 +0530

    to check if an integer is prime or not

---
+++ while_loop_q10.py
@@ -0,0 +1,14 @@
+n=int(input("Enter the number: "))
+if n>-1 and n<2:
+    print(n,"is neither prime nor composite")
+else:
+    j=2
+    prime=0
+    while j<n:
+        if n%j==0:
+            prime=1
+        j+=1
+    if prime==0:
+        print(n," is prime")
+    else:
+        print(n,"is a composite number")
