ef1d2dfc5094132eacc79d2375d1ab6b5d9ec447
edahnXC <edahnxc@gmail.com>
2023-02-05 19:33:53 +0530

    to print the elements of a stack in reverse order

--- while_loop_30.py
+++ while_loop_30.py
@@ -1,8 +1,17 @@
-n=int(input("n: "))
-i="2"
-j=0
-while j<n:
-    print(i,end=',')
-    i+="2"
-    j+=1
-print(type(i))
+#TO PRINT THE SEQUENCE OF 2,22,222,2222......N TERMS
+# n=int(input("n: "))
+# i="2"
+# j=0
+# while j<n:
+#     print(i,end=',')
+#     i+="2"
+#     j+=1
+# print(type(i))
+
+#to print the elements inside a stack in a reverse order
+s=['R','A','U','N','E','E','T']   
+i=s.index(s[-1])
+while i>=0:
+    print(s[i],end=',')
+    i-=1
+
